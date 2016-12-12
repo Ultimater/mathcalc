20ce02decf306fee0a9b957e87d4603b93fc1bc0
Kevin Yarmak <Ultimater@gmail.com>
2016-12-12 03:22:19 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * This is a Reverse Polish Notation Calculator, feel free to edit to your liking.
+ * This is a Reverse Polish Notation Calculator, feel free to edit it to your liking.
  *
  * Copyright (c) 2016 ultimater at gmail dot com
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -16,6 +16,7 @@ class MathCalculator
     {
         $level = 0;
         $tokens = array();
+        // loop through the string one character at a time and create an array of tokens
         foreach( str_split($str) as $c )
         {
             if( $level < 0 ) throw new Exception("Mismatched parentheses.");
@@ -35,17 +36,39 @@ class MathCalculator
             throw new Exception("Unsupported Character: ".$c);
         }
         if( $level != 0 ) throw new Exception( sprintf("Missing %s parentheses.", $level > 0 ? 'right' : 'left') );
+        return $this->cleanTokens($tokens);
+
+    } // tokenize
+
+    private function cleanTokens($tokens)
+    {
         $outArray = array();
+        // Loop through our tokens array, get rid of space, and validate our tokens.
         foreach( $tokens as $token )
         {
             if( $token == ' ' )continue;
-            if( preg_match("#^[/+*()-]$#", $token) ){ $outArray[] = $token;continue; }
-            if( preg_match("#^\\d+\\.+$#", $token) ) throw new Exception("Numbers cannot end with a decimal point: ". $token);
-            if( !$this->isNumeric($token) ) throw new Exception("Expected a numeric token: ". $token); //e.g. 1.2.3.4
+            $previousToken = end($outArray); // this is FALSE for an empty array
+            // implied multiplication. e.g. 3( and )( are implied, while (( and +( are not
+            if( $token == '(' && $previousToken !== FALSE && !$this->isOperator($previousToken) && $previousToken != '(' )
+            {
+                $outArray[] = '*';
+            }
+            // implied multiplication. e.g. )3 and )( are implied, while )) and )+ are not
+            elseif( $previousToken == ')' && !$this->isOperator($token) && $token != ')' )
+            {
+                $outArray[] = '*';
+            }
+            if( $this->isOperator($token) || $token == '(' || $token == ')' ){ $outArray[] = $token;continue; }
+            if( preg_match("#^\\d+\\.+$#", $token) )
+                throw new Exception("Numbers cannot end with a decimal point: ". $token);
+            if( !$this->isNumeric($token) )
+                throw new Exception("Expected a numeric token: ". $token); //e.g. 1.2.3.4
+            if( $this->isNumeric($token) && $previousToken !== FALSE && $this->isNumeric($previousToken))
+                throw new Exception("Cannot have two numeric tokens in a row: $previousToken $token"); //e.g. 12.34 56.78
             $outArray[] = $token;
         }
         return $outArray;
-    } // tokenize
+    } // cleanTokens
 
     protected function isOperator($token)
     {
@@ -55,7 +78,7 @@ class MathCalculator
     // Unlike the built-in is_numeric, this function doesn't allow leading space, or a leading sign.
     protected function isNumeric($token)
     {
-        return preg_match("#^[0-9]+(?:\\.[0-9]+)?$#", $token);
+        return preg_match("#^[0-9]+(?:\\.[0-9]+)?$|^\\.[0-9]+$#", $token);
     } // isNumeric
 
     protected function getPrecedence($op)
@@ -75,7 +98,9 @@ class MathCalculator
             if( $this->isNumeric($token) ){ $outputQueue[] = $token; continue; }
             if( $this->isOperator($token) )
             {
-                while( ( $op = end($operatorStack) ) && $op !== NULL && $this->isOperator($op) && $this->getPrecedence($token) <= $this->getPrecedence($op))
+                // We only want to loop when the operator stack isn't empty, and the last element of the array (i.e. on top of the stack) is an operator
+                // and only if its precedence is higher than the token (which is also an operator) we're trying to decide what to do with.
+                while( ( $op = end($operatorStack) ) !== FALSE && $this->isOperator($op) && $this->getPrecedence($token) <= $this->getPrecedence($op))
                 {
                     $outputQueue[] = array_pop($operatorStack);
                 }
@@ -91,6 +116,7 @@ class MathCalculator
                 }
             }
         }
+        // If there's any operators left in the operator stack, then we move it over to the output queue.
         while( ( $op = array_pop($operatorStack) ) !== NULL )
         {
             if($op == '(' || $op == ')') throw new Exception("Mismatched parentheses.");
@@ -101,6 +127,7 @@ class MathCalculator
 
     protected function compute($ar)
     {
+        $stack = array();
         foreach( $ar as $token )
         {
             if( $this->isNumeric($token) ) {$stack[] = $token;continue;}
@@ -184,7 +211,7 @@ class MathCalculator
 
 <form method="post">
 <table>
-<tr><td><label for="infixExpr">Infix<label></td><td><input type="text" id="infixExpr" name="infixExpr" value="<?= htmlentities($infix) ?>"><br>(e.g. 18 / (9 + 9) + 7 - 5)</td></tr>
+<tr><td><label for="infixExpr">Infix<label></td><td><input type="text" id="infixExpr" name="infixExpr" value="<?= htmlentities($infix) ?>"><br>e.g. 18 / (9 + 9) + 7 - 5</td></tr>
 <tr><td colspan="2" style="text-align:right"><button type="submit" name="input" value="infix">Compute</button></td></tr>
 <tr><td><label for="postfixExp">Postfix</label></td><td><input type="text" id="postfixExpr" readonly="readonly" name="postfixExpr" value="<?= htmlentities($postfix) ?>"></td></tr>
 </table>
